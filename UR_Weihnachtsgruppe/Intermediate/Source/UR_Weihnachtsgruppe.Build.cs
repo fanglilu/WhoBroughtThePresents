4b4f1674690f557212663a50eee84003dc0303a4
Alexandra Ginger <69037285+Alexandra-Ginger@users.noreply.github.com>
2022-02-28 09:18:32 +0100

    Einstellungen Packaging

---
+++ UR_Weihnachtsgruppe/Intermediate/Source/UR_Weihnachtsgruppe.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class UR_Weihnachtsgruppe : ModuleRules
+{
+	public UR_Weihnachtsgruppe(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
